d0758578908e94279d85e2318d9ca366ab965882
raposofrct <raposo.frct@gmail.com>
2022-02-11 12:12:40 -0300

    updating database automatically using crontab

--- ETL/command.sh
+++ ETL/command.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+export SSH_AGENT_PID=`ps -a | grep ssh-agent | grep -o -e [0-9][0-9][0-9][0-9]`
+export SSH_AUTH_SOCK=`find /tmp/ -path '*keyring-*' -name '*ssh*' -print 2>/dev/null` 
+
 # Execute Script (Collect more data)
 /Users/nando/.pyenv/versions/3.9.2/envs/dsaodevenv/bin/python /Users/nando/Comunidade\ DS/ds_ao_dev/ETL/script.py
 
